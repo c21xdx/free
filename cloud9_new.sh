a4842ee1c50bfd75ce9be82acca723e33bdf74a2
c21xdx <c21xdx@gmail.com>
2020-05-05 23:46:51 +0800

    Update cloud9_new.sh

--- cloud9_new.sh
+++ cloud9_new.sh
@@ -11,4 +11,4 @@ curl -sL https://ibm.biz/idt-installer | bash
 curl https://get.okteto.com -sSfL | sh
 # 安装flyctl
 curl -L https://getfly.fly.dev/install.sh | sh
-rclone copy gdrive:mycloud9 /c9ws/ --config /etc/c9conf/my.conf
+rclone copy gdrive:mycloud9 /c9ws/ --config /etc/c9conf/gd.conf
