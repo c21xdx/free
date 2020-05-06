139680fa2446781a1f578061499b7d4acb3b466b
c21xdx <c21xdx@gmail.com>
2020-05-06 22:55:41 +0800

    Create cloud9_rclone.sh

---
+++ cloud9_rclone.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+apt-get update
+# 安装 cf cli
+curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
+mv cf /usr/local/bin
+sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
+# 安装 ibmcloud cli
+curl -sL https://ibm.biz/idt-installer | bash
+# 安装OKTETO CLI
+curl https://get.okteto.com -sSfL | sh
+# 安装flyctl
+curl -L https://getfly.fly.dev/install.sh | sh
+# GD拷回
+rclone copy gdrive:mycloud9 /c9ws/ --config /etc/c9conf/gd.conf
