e35ce0da559f42444bdc0ad4553fab5b16f18d2a
c21xdx <c21xdx@gmail.com>
2020-05-31 19:18:21 +0800

    Update cloud9_flyio.sh

--- cloud9_flyio.sh
+++ cloud9_flyio.sh
@@ -5,6 +5,8 @@ apt-get update
 curl -L "https://packages.cloudfoundry.org/stable?release=linux64-binary&source=github" | tar -zx
 mv cf /usr/local/bin
 sudo curl -o /usr/share/bash-completion/completions/cf https://raw.githubusercontent.com/cloudfoundry/cli/master/ci/installers/completion/cf
+# 安装 ibmcloud cli
+curl -fsSL https://clis.cloud.ibm.com/install/linux | sh
 # 安装OKTETO CLI
 curl https://get.okteto.com -sSfL | sh
 # 安装flyctl
