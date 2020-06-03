55e58e189df89cd672314a537a3ed61c09ae05e5
c21xdx <c21xdx@gmail.com>
2020-06-03 16:30:56 +0800

    Update cloud9_flyio.sh

--- cloud9_flyio.sh
+++ cloud9_flyio.sh
@@ -2,3 +2,8 @@
 
 # GD拷回
 rclone copy gdrive:mycloud9 /c9ws/ --config /etc/c9conf/gd.conf
+#设置crontab
+env >> /etc/default/locale
+touch /var/spool/cron/crontabs/root
+echo "*/10 * * * * /bin/sh /c9ws/rclone/sync.sh >/dev/null 2>&1" >>/var/spool/cron/crontabs/root
+service cron restart
