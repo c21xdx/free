62916130bf84910215bc954a2ba14505e8ef63b3
c21xdx <c21xdx@gmail.com>
2021-02-14 22:50:32 +0800

    Update cloud9_flyio.sh

--- cloud9_flyio.sh
+++ cloud9_flyio.sh
@@ -6,4 +6,5 @@ output=`rclone copy gdrive:mycloud9 /c9ws/ --config /etc/c9conf/gd.conf`
 env >> /etc/default/locale
 touch /var/spool/cron/crontabs/root
 echo "*/10 * * * * /bin/sh /c9ws/rclone/sync.sh >/dev/null 2>&1" >>/var/spool/cron/crontabs/root
+chmod 600 /var/spool/cron/crontabs/root
 service cron restart
