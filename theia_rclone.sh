8d3a450e074224042ab43b165a67179ace5ffd44
c21xdx <c21xdx@gmail.com>
2021-12-02 09:19:55 +0800

    Update theia_rclone.sh

--- theia_rclone.sh
+++ theia_rclone.sh
@@ -2,3 +2,13 @@
 
 # update Golang
 curl -fsSL https://go.dev/dl/go1.17.3.linux-amd64.tar.gz | tar -C /home -xzv
+
+# GD备份回来
+output=`rclone copy gdrive:theiaide/GoCode /home/project/Gocode --config /etc/goconf/gd.conf`
+
+#设置crontab
+env >> /etc/default/locale
+touch /var/spool/cron/crontabs/root
+echo "*/10 * * * * /bin/sh /home/project/rclone/sync.sh >/dev/null 2>&1" >>/var/spool/cron/crontabs/root
+chmod 600 /var/spool/cron/crontabs/root
+service cron restart
