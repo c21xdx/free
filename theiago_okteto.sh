31894e65f24498fc4369146f39f3e1e1000e30f4
c21xdx <c21xdx@gmail.com>
2021-12-03 11:48:32 +0800

    Update theiago_okteto.sh

--- theiago_okteto.sh
+++ theiago_okteto.sh
@@ -4,3 +4,9 @@ apt-get update
 
 # update Golang
 curl -fsSL https://go.dev/dl/go1.17.3.linux-amd64.tar.gz | tar -C /home -xzv
+
+#rclone
+cd /home/project/rclone/rclone-*-linux-amd64
+cp rclone /usr/bin/
+chown root:root /usr/bin/rclone
+chmod 755 /usr/bin/rclone
