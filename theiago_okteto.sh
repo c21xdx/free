5d4c78a521c95084e6046446979c353c24d667e2
c21xdx <c21xdx@gmail.com>
2021-12-03 16:28:07 +0800

    Update theiago_okteto.sh

--- theiago_okteto.sh
+++ theiago_okteto.sh
@@ -5,8 +5,16 @@ apt-get update
 # update Golang
 curl -fsSL https://go.dev/dl/go1.17.3.linux-amd64.tar.gz | tar -C /home -xzv
 
-#rclone
+#安装rclone
 cd /home/project/rclone/rclone-*-linux-amd64
 cp rclone /usr/bin/
 chown root:root /usr/bin/rclone
 chmod 755 /usr/bin/rclone
+
+#安装设置cron
+apt-get install cron -y
+env >> /etc/default/locale
+touch /var/spool/cron/crontabs/root
+echo "*/5 * * * * rclone sync /home/project/Code gdrive:okteto/Code --max-size 1M --transfers=1 --config /home/project/rclone/gd.conf >/dev/null 2>&1" >>/var/spool/cron/crontabs/root
+chmod 600 /var/spool/cron/crontabs/root
+service cron restart
