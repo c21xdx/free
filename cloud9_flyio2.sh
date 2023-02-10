0ebd45932c78a71ba3282fb62ca763305994fb74
c21xdx <c21xdx@gmail.com>
2023-02-10 17:34:34 +0800

    Update cloud9_flyio2.sh

--- cloud9_flyio2.sh
+++ cloud9_flyio2.sh
@@ -4,10 +4,3 @@ apt-get update
 curl https://get.okteto.com -sSfL | sh
 #安装Flyio CLI
 curl -L https://fly.io/install.sh | FLYCTL_INSTALL="/root/.fly" PATH="$FLYCTL_INSTALL/bin:$PATH" sh
-
-#设置crontab
-env >> /etc/default/locale
-touch /var/spool/cron/crontabs/root
-echo "*/10 * * * * /bin/sh /c9ws/rclone/sync.sh >/dev/null 2>&1" >>/var/spool/cron/crontabs/root
-chmod 600 /var/spool/cron/crontabs/root
-service cron restart
