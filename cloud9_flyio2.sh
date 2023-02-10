c25818955984ebe54f37e231ffe7fb50b6f5ac62
c21xdx <c21xdx@gmail.com>
2023-02-10 18:43:18 +0800

    Update cloud9_flyio2.sh

--- cloud9_flyio2.sh
+++ cloud9_flyio2.sh
@@ -3,4 +3,6 @@ apt-get update
 #安装 Okteto CLI
 curl https://get.okteto.com -sSfL | sh
 #安装Flyio CLI
-curl -L https://fly.io/install.sh | FLYCTL_INSTALL="/root/.fly" PATH="$FLYCTL_INSTALL/bin:$PATH" sh
+curl -L https://fly.io/install.sh | FLYCTL_INSTALL="/root/.fly" | PATH="$FLYCTL_INSTALL/bin:$PATH" sh
+
+npm i -g vercel
